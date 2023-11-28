f2819a182724e9f5f31dcf0c2b2dacba0504b005
EdaCoder <847432003@qq.com>
2023-11-28 16:18:16 +0800

    自定义控件

--- CandyControls/Controls/CandyToggle.cs
+++ CandyControls/Controls/CandyToggle.cs
@@ -1,9 +1,63 @@
-﻿using System.Windows;
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace CandyControls
 {
     public class CandyToggle : ListBox
     {
+        public IEnumerable ModelSource
+        {
+            get { return (IEnumerable)GetValue(ModelSourceProperty); }
+            set { SetValue(ModelSourceProperty, value); }
+        }
+        public static readonly DependencyProperty ModelSourceProperty =
+            DependencyProperty.Register("ModelSource", typeof(IEnumerable), typeof(CandyToggle), new PropertyMetadata(OnModelChanged));
+
+
+        public Brush SelectedBrush
+        {
+            get { return (Brush)GetValue(SelectedBrushProperty); }
+            set { SetValue(SelectedBrushProperty, value); }
+        }
+        public static readonly DependencyProperty SelectedBrushProperty =
+            DependencyProperty.Register("SelectedBrush", typeof(Brush), typeof(CandyToggle), new PropertyMetadata(new SolidColorBrush(Color.FromArgb(0x70,0x80,0xCB,0xCC))));
+
+        public Brush MouseOverBrush
+        {
+            get { return (Brush)GetValue(MouseOverBrushProperty); }
+            set { SetValue(MouseOverBrushProperty, value); }
+        }
+        public static readonly DependencyProperty MouseOverBrushProperty =
+            DependencyProperty.Register("MouseOverBrush", typeof(Brush), typeof(CandyToggle), new PropertyMetadata(new SolidColorBrush(Color.FromArgb(0x70, 0xE0, 0xE0, 0xDB))));
+
+        public Brush BackBrush
+        {
+            get { return (Brush)GetValue(BackBrushProperty); }
+            set { SetValue(BackBrushProperty, value); }
+        }
+        public static readonly DependencyProperty BackBrushProperty =
+            DependencyProperty.Register("BackBrush", typeof(Brush), typeof(CandyToggle), new PropertyMetadata(new SolidColorBrush(Color.FromArgb(0x40, 0xB3, 0xB3,0xB3))));
+
+        public Brush ForeBrush
+        {
+            get { return (Brush)GetValue(ForeBrushProperty); }
+            set { SetValue(ForeBrushProperty, value); }
+        }
+        public static readonly DependencyProperty ForeBrushProperty =
+            DependencyProperty.Register("ForeBrush", typeof(Brush), typeof(CandyToggle), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(0xFD, 0xF5, 0xE6))));
+
+
+        private static void OnModelChanged(DependencyObject dp, DependencyPropertyChangedEventArgs eve)
+        {
+            var toggle = dp as CandyToggle;
+            ((ObservableCollection<string>)eve.NewValue).ToList().ForEach(item =>
+            {
+                toggle.AddChild(new CandyToggleItem { Content = item });
+            });
+        }
     }
 }
--- WPFControl/ControlDemo/CandyToggleDemo.xaml.cs
+++ WPFControl/ControlDemo/CandyToggleDemo.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Collections.ObjectModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace WPFControl.ControlDemo
 {
@@ -24,5 +13,15 @@ namespace WPFControl.ControlDemo
         {
             InitializeComponent();
         }
+    }
+    public partial class CandyToggleDemoVM : ObservableObject
+    {
+        public CandyToggleDemoVM()
+        {
+            Data = new ObservableCollection<string> { "测试1", "测试2" };
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<string> _Data;
     }
 }
