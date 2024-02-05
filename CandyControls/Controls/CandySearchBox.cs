23e2312c56616aea1e67671e7ee639eb257ec00f
EmilyEdna <847432003@qq.com>
2024-02-05 16:00:58 +0800

    搜索框

--- CandyControls/Controls/CandySearchBox.cs
+++ CandyControls/Controls/CandySearchBox.cs
@@ -1,9 +1,15 @@
 ﻿using CandyControls.ControlsModel.Enums;
+using CandyControls.ControlsModel.Thicks;
 using System;
+using System.Collections;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
 using System.Xml.Linq;
 
 namespace CandyControls
@@ -19,14 +25,19 @@ namespace CandyControls
 
         Button PART_CLEAR_BTN;
         ScrollViewer PART_CONTENTHOST;
+        Border PART_TXT;
+        TextBlock PART_SEARCH;
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
+            PART_SEARCH = (TextBlock)this.Template.FindName("PART_SEARCH", this);
+            PART_TXT = (Border)this.Template.FindName("PART_TXT", this);
             PART_CLEAR_BTN = (Button)this.Template.FindName("PART_CLEAR_BTN", this);
             PART_CONTENTHOST = (ScrollViewer)this.Template.FindName("PART_ContentHost", this);
             PART_CLEAR_BTN.Click += ClearEvent;
             PART_CONTENTHOST.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
             this.TextChanged += TextEvent;
+            PART_SEARCH.PreviewMouseLeftButtonDown += SearchIconEvent;
         }
 
         #region Property
@@ -45,6 +56,7 @@ namespace CandyControls
         }
         public static readonly DependencyProperty PlacmentProperty =
             DependencyProperty.Register("Placment", typeof(EPlacment), typeof(CandySearchBox), new FrameworkPropertyMetadata(EPlacment.Right, OnPlacementChanged));
+
         public double BoxWidth
         {
             get { return (double)GetValue(BoxWidthProperty); }
@@ -68,12 +80,72 @@ namespace CandyControls
         }
         public static readonly DependencyProperty MoveBrushProperty =
             DependencyProperty.Register("MoveBrush", typeof(Brush), typeof(CandySearchBox), new PropertyMetadata(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#d3d3d3"))));
+
+        public bool UseAutoComplete
+        {
+            get { return (bool)GetValue(UseAutoCompleteProperty); }
+            set { SetValue(UseAutoCompleteProperty, value); }
+        }
+        public static readonly DependencyProperty UseAutoCompleteProperty =
+            DependencyProperty.Register("UseAutoComplete", typeof(bool), typeof(CandySearchBox), new PropertyMetadata(false));
+
+        public ItemsPanelTemplate ItemsPanel
+        {
+            get { return (ItemsPanelTemplate)GetValue(ItemsPanelProperty); }
+            set { SetValue(ItemsPanelProperty, value); }
+        }
+        public static readonly DependencyProperty ItemsPanelProperty =
+            DependencyProperty.Register("ItemsPanel", typeof(ItemsPanelTemplate), typeof(CandySearchBox), new PropertyMetadata(new ItemsPanelTemplate(new FrameworkElementFactory(typeof(StackPanel)))));
+
+        public DataTemplate AutoComplete
+        {
+            get { return (DataTemplate)GetValue(AutoCompleteProperty); }
+            set { SetValue(AutoCompleteProperty, value); }
+        }
+        public static readonly DependencyProperty AutoCompleteProperty =
+            DependencyProperty.Register("AutoComplete", typeof(DataTemplate), typeof(CandySearchBox), new PropertyMetadata(default));
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(CandySearchBox), new PropertyMetadata(default));
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(CandySearchBox), new PropertyMetadata(default));
+
+        public Brush MaskFill
+        {
+            get { return (Brush)GetValue(MaskFillProperty); }
+            set { SetValue(MaskFillProperty, value); }
+        }
+        public static readonly DependencyProperty MaskFillProperty =
+            DependencyProperty.Register("MaskFill", typeof(Brush), typeof(CandySearchBox), new PropertyMetadata(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#20DADADA"))));
+        public Style ListStyle
+        {
+            get { return (Style)GetValue(ListStyleProperty); }
+            set { SetValue(ListStyleProperty, value); }
+        }
+        public static readonly DependencyProperty ListStyleProperty =
+            DependencyProperty.Register("ListStyle", typeof(Style), typeof(CandySearchBox), new PropertyMetadata(default));
         #endregion
 
         #region Method
         private void ClearEvent(object sender, RoutedEventArgs e)
         {
+            var box = (sender as Button).FindParent<CandySearchBox>();
             this.Text = string.Empty;
+            if (box.UseAutoComplete)
+            {
+                box.Command?.Execute(this.Text);
+            }
         }
 
         private void TextEvent(object sender, TextChangedEventArgs e)
@@ -81,7 +153,55 @@ namespace CandyControls
             var box = (sender as CandySearchBox);
             box.ScrollToHorizontalOffset(box.VisualOffset.X);
             TextActionChanged?.Invoke(box, e);
+            if (box.UseAutoComplete)
+            {
+                box.Command?.Execute(this.Text);
+
+                Grid panal = new Grid
+                {
+                    Height = 150,
+                };
+                panal.Children.Add(new Rectangle
+                {
+                    Height = panal.Height,
+                    Width = box.BoxWidth,
+                    Fill = MaskFill,
+                });
+                panal.Children.Add(new ListBox
+                {
+                    Style=box.ListStyle,
+                    Height = panal.Height,
+                    ItemsPanel = ItemsPanel,
+                    ItemsSource = box.ItemsSource,
+                    ItemTemplate = AutoComplete
+                });
+                Popup popup = new Popup
+                {
+                    Placement = PlacementMode.Bottom,
+                    PlacementTarget = PART_TXT,
+                    AllowsTransparency = true,
+                    StaysOpen = false,
+                    Height = panal.Height,
+                    Width = panal.Width,
+                    Child = panal
+                };
+                popup.IsOpen = true;
+                popup.Closed += delegate
+                {
+                    CloseAnime();
+                };
+                popup.LostFocus += delegate
+                {
+                    popup.IsOpen = false;
+                };
+            }
         }
+
+        private void SearchIconEvent(object sender, MouseButtonEventArgs e)
+        {
+            OpenAnime();
+        }
+
         private static void OnPlacementChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var box = (d as CandySearchBox);
@@ -89,6 +209,50 @@ namespace CandyControls
                 box.Point = new Point(1, 1);
             else
                 box.Point = new Point(0, 0);
+        }
+        #endregion
+
+        #region Anime
+        Storyboard Close;
+        Storyboard Open;
+        private void CloseAnime()
+        {
+            Close = new Storyboard();
+            DoubleAnimationUsingKeyFrames Revolve = new DoubleAnimationUsingKeyFrames();
+            Storyboard.SetTarget(Revolve, PART_TXT);
+            Storyboard.SetTargetProperty(Revolve, new PropertyPath("(UIElement.RenderTransform).(TransformGroup.Children)[0].(ScaleTransform.ScaleX)"));
+            Revolve.KeyFrames.Add(new EasingDoubleKeyFrame(1, TimeSpan.FromSeconds(0)));
+            Revolve.KeyFrames.Add(new EasingDoubleKeyFrame(0, TimeSpan.FromSeconds(1)));
+
+            DoubleAnimationUsingKeyFrames Revolve2 = new DoubleAnimationUsingKeyFrames();
+            Storyboard.SetTarget(Revolve2, PART_TXT);
+            Storyboard.SetTargetProperty(Revolve2, new PropertyPath("(UIElement.Opacity)"));
+            Revolve2.KeyFrames.Add(new EasingDoubleKeyFrame(1, TimeSpan.FromSeconds(0)));
+            Revolve2.KeyFrames.Add(new EasingDoubleKeyFrame(0, TimeSpan.FromSeconds(1)));
+
+            Close.Children.Add(Revolve);
+            Close.Children.Add(Revolve2);
+            Close.Begin();
+        }
+
+        private void OpenAnime()
+        {
+            Open = new Storyboard();
+            DoubleAnimationUsingKeyFrames Revolve = new DoubleAnimationUsingKeyFrames();
+            Storyboard.SetTarget(Revolve, PART_TXT);
+            Storyboard.SetTargetProperty(Revolve, new PropertyPath("(UIElement.RenderTransform).(TransformGroup.Children)[0].(ScaleTransform.ScaleX)"));
+            Revolve.KeyFrames.Add(new EasingDoubleKeyFrame(0, TimeSpan.FromSeconds(0)));
+            Revolve.KeyFrames.Add(new EasingDoubleKeyFrame(1, TimeSpan.FromSeconds(1)));
+
+            DoubleAnimationUsingKeyFrames Revolve2 = new DoubleAnimationUsingKeyFrames();
+            Storyboard.SetTarget(Revolve2, PART_TXT);
+            Storyboard.SetTargetProperty(Revolve2, new PropertyPath("(UIElement.Opacity)"));
+            Revolve2.KeyFrames.Add(new EasingDoubleKeyFrame(0, TimeSpan.FromSeconds(0)));
+            Revolve2.KeyFrames.Add(new EasingDoubleKeyFrame(1, TimeSpan.FromSeconds(1)));
+
+            Open.Children.Add(Revolve);
+            Open.Children.Add(Revolve2);
+            Open.Begin();
         }
         #endregion
     }
--- WPFControl/ControlDemo/CandySearchBoxDemo.xaml.cs
+++ WPFControl/ControlDemo/CandySearchBoxDemo.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,42 @@ namespace WPFControl.ControlDemo
         public CandySearchBoxDemo()
         {
             InitializeComponent();
+            this.DataContext = new CandySearchBoxDemoDemoVM();
         }
+    }
+    public partial class CandySearchBoxDemoDemoVM : ObservableObject
+    {
+        public CandySearchBoxDemoDemoVM()
+        {
+            Data = new ObservableCollection<SearchModel>();
+        }
+        [ObservableProperty]
+        private ObservableCollection<SearchModel> _Data;
+
+        [RelayCommand]
+        public void Search(string input)
+        {
+            if (input == "1")
+            {
+                Data.Add(new SearchModel { Id = 1, Name = "1" });
+                Data.Add(new SearchModel { Id = 2, Name = "2" });
+                Data.Add(new SearchModel { Id = 3, Name = "3" });
+                Data.Add(new SearchModel { Id = 4, Name = "4" });
+                Data.Add(new SearchModel { Id = 5, Name = "5" });
+                Data.Add(new SearchModel { Id = 6, Name = "6" });
+                Data.Add(new SearchModel { Id = 7, Name = "7" });
+                Data.Add(new SearchModel { Id = 8, Name = "8" });
+                Data.Add(new SearchModel { Id = 9, Name = "9" });
+                Data.Add(new SearchModel { Id = 10, Name = "10" });
+            }
+            else
+                Data = new ObservableCollection<SearchModel>();
+        }
+    }
+
+    public class SearchModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
