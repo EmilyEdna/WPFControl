47f0e9f389664291d3a251597be7174ea56633c4
EmilyEdna <847432003@qq.com>
2024-07-01 15:20:46 +0800

    新增属性

--- CandyControls/Controls/CandyWindow.cs
+++ CandyControls/Controls/CandyWindow.cs
@@ -50,13 +50,21 @@ namespace CandyControls
         public static readonly DependencyProperty MenuBoxProperty =
             DependencyProperty.Register("MenuBox", typeof(Control), typeof(CandyWindow), new PropertyMetadata(default));
 
-        public IEnumerable ItemSource
+        public IEnumerable MenuItemSource
         {
-            get { return (IEnumerable)GetValue(ItemSourceProperty); }
-            set { SetValue(ItemSourceProperty, value); }
+            get { return (IEnumerable)GetValue(MenuItemSourceProperty); }
+            set { SetValue(MenuItemSourceProperty, value); }
         }
-        public static readonly DependencyProperty ItemSourceProperty =
-            DependencyProperty.Register("ItemSource", typeof(IEnumerable), typeof(CandyWindow), new PropertyMetadata(default));
+        public static readonly DependencyProperty MenuItemSourceProperty =
+            DependencyProperty.Register("MenuItemSource", typeof(IEnumerable), typeof(CandyWindow), new PropertyMetadata(default));
+
+        public IEnumerable SearchItemSource
+        {
+            get { return (IEnumerable)GetValue(SearchItemSourceProperty); }
+            set { SetValue(SearchItemSourceProperty, value); }
+        }
+        public static readonly DependencyProperty SearchItemSourceProperty =
+            DependencyProperty.Register("SearchItemSource", typeof(IEnumerable), typeof(CandyWindow), new PropertyMetadata(default));
         #endregion
 
         public override void OnApplyTemplate()
