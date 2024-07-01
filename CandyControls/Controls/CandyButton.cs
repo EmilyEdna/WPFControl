f7194d8d4a79e9665cbc7de89cc733c1561aff43
EmilyEdna <847432003@qq.com>
2024-07-01 09:44:14 +0800

    多样化按钮

--- CandyControls/Controls/CandyButton.cs
+++ CandyControls/Controls/CandyButton.cs
@@ -21,5 +21,27 @@ namespace CandyControls
         /// </summary>
         public static readonly DependencyProperty ButtonTypeProperty =
             DependencyProperty.Register("ButtonType", typeof(ECatagory), typeof(CandyButton), new PropertyMetadata(ECatagory.Primary));
+
+
+        /// <summary>
+        /// 是否启用斜边框
+        /// </summary>
+        public bool EnableBeveleBorder
+        {
+            get { return (bool)GetValue(EnableBeveleBorderProperty); }
+            set { SetValue(EnableBeveleBorderProperty, value); }
+        }
+
+        public static readonly DependencyProperty EnableBeveleBorderProperty =
+            DependencyProperty.Register("EnableBeveleBorder", typeof(bool), typeof(CandyButton), new PropertyMetadata(false));
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            Border.CornerRadiusProperty.AddOwner(typeof(CandyButton), new PropertyMetadata(new CornerRadius()));
     }
 }
