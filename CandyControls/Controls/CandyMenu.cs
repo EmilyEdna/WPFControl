1783eb9267bfbc59bb39e6b36f962ab97f7e4911
EmilyEdna <847432003@qq.com>
2024-07-01 14:47:37 +0800

    调整样式

---
+++ CandyControls/Controls/CandyMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CandyControls
+{
+    public class CandyMenu : Menu
+    {
+        public CandyMenu()
+        {
+            this.SetResourceReference(CandyMenu.StyleProperty, nameof(CandyMenu));
+        }
+    }
+
+    public class CandyMenuItem : MenuItem
+    {
+        public CandyMenuItem()
+        {
+            this.SetResourceReference(CandyMenuItem.StyleProperty, nameof(CandyMenuItem));
+        }
+    }
+}
