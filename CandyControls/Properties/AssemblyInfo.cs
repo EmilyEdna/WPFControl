bedbbe5d9fa324bd6b49a3a106a90709da44f6e2
EmilyEdna <847432003@qq.com>
2024-07-03 16:23:34 +0800

    打包

--- CandyControls/Properties/AssemblyInfo.cs
+++ CandyControls/Properties/AssemblyInfo.cs
@@ -1,4 +1,13 @@
-﻿using System.Windows.Markup;
-
+﻿using System.Reflection;
+using System.Runtime.Versioning;
+using System.Windows.Markup;
 
 [assembly: XmlnsDefinition("http://edacoder.github.io", "CandyControls")]
+[assembly: AssemblyDescription("847432003@qq.com")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyCompany("EmilyEdna")]
+[assembly: AssemblyTitle("CandyControls")]
+[assembly: AssemblyProduct("CandyControls")]
+[assembly: AssemblyCopyright("https://github.com/EmilyEdna")]
+[assembly: SupportedOSPlatform("windows")]
