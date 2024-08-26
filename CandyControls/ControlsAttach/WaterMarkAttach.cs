3940190b87b3490859c6829f06f947e5e25c3361
EmilyEdna <847432003@qq.com>
2024-08-26 22:15:24 +0800

    新增样式和水印

---
+++ CandyControls/ControlsAttach/WaterMarkAttach.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using XExten.Advance.LinqFramework;
+
+namespace CandyControls
+{
+    public static class WaterMarkAttach
+    {
+        public static readonly DependencyProperty PlaceholderProperty =
+            DependencyProperty.RegisterAttached("Placeholder", typeof(string), typeof(WaterMarkAttach),
+                new FrameworkPropertyMetadata(default(string), (sender, args) => {
+                    var box = (TextBox)sender;
+                    if (!args.NewValue.ToString().IsNullOrEmpty())
+                    {
+                        box.Foreground = Brushes.Gray;
+                        box.Background = Brushes.Transparent;
+                        box.Text = args.NewValue.ToString();
+
+                        box.GotFocus += (s, e) =>
+                        {
+                            if (box.Text == args.NewValue.ToString())
+                            {
+                                box.Text = string.Empty;
+                                box.Background = null;
+                            }
+
+                        };
+
+                        box.LostFocus += (s, e) => 
+                        {
+                            box.Foreground = Brushes.Gray;
+                            box.Background = Brushes.Transparent;
+                            box.Text = args.NewValue.ToString();
+                        };
+                    }
+                }));
+
+        public static void SetPlaceholder(DependencyObject element, string value)
+            => element.SetValue(PlaceholderProperty, value);
+
+        public static string GetPlaceholder(DependencyObject element)
+            => (string)element.GetValue(PlaceholderProperty);
+
+    }
+}
